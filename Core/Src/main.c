9fe1f9320b335e72bbd0a7e489cee521a8206e38
LiTang <litang0617@outlook.com>
2024-01-14 14:31:38 +0800

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -25,6 +25,7 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
+#include "LCD.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -105,6 +106,7 @@ int main(void)
   MX_SPI2_Init();
   MX_CAN_Init();
   /* USER CODE BEGIN 2 */
+
 
   /* USER CODE END 2 */
   /* Infinite loop */
